18a6e181833dd5ea948f5b8e6ed1b146903b09a5
mdomanski90 <mateuszdomanski@hotmail.com>
2024-05-27 02:05:21 +0200

    0.91

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
-const = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
